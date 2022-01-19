2bb4f649bc800e5e5e2824f060e36f067c06c2c1
Gabriel_Saliba <gabriel.saliba@posher.app>
2022-01-19 20:31:41 -0300

    ✨ - list items

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,20 @@
 import express, { application } from "express";
+import knex from "./database/connection";
 
 const routes = express.Router();
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "Hello World!" });
+routes.get("/items", async (request, response) => {
+  const items = await knex("items").select("*");
+  const requestHost = request.get('host')
+  const requestProtocol = request.protocol
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `${requestProtocol}://${requestHost}/uploads/${item.image}`
+    };
+  })
+
+  return response.json(serializedItems);
 });
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,10 +3,15 @@ import routes from "./routes";
 import path from 'path'
 
 const app = express();
+const port = '3333'
 
 app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
-app.listen(3333);
+app.listen(port);
+
+console.log(`\n 🌱   ECOLETA   ♻️`);
+console.log(`\n\n 🚀 Server is now running!`);
+console.log(` ⛩️ Port: ${port}`);
