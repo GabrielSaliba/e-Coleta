c2ddfb43b8ee4c545b90a3f521281c47f4853d47
Gabriel_Saliba <gabriel.saliba@posher.app>
2022-01-23 02:19:46 -0300

    🤡 - mock point image

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -25,7 +25,8 @@ class PointsController {
     const trx = await knex.transaction();
 
     const point = {
-      image: "image-fake",
+      image:
+        "https://images.unsplash.com/photo-1550989460-0adf9ea622e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bWFya2V0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
       name,
       email,
       whatsapp,
