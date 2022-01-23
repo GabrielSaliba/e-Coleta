00bc2ca27c9013269e3b31a64cc1521616974f41
Gabriel_Saliba <gabriel.saliba@posher.app>
2022-01-23 02:15:45 -0300

    🏗️ - add cors to server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import routes from "./routes";
-import path from 'path'
+import path from "path";
+import cors from "cors";
 
 const app = express();
-const port = '3333'
+const port = "3333";
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(port);
 
