89ef7290ab557cbe74160527ebe78e0a44b3069b
Gabriel_Saliba <gabriel.saliba@posher.app>
2022-01-23 02:10:20 -0300

    ✨ - filter points by items

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -6,21 +6,23 @@ class PointsController {
     const pointId = request.params.id;
 
     const point = await knex("points").where("id", pointId).first();
-    const items = await knex('items')
-    .join('point_items', 'items.id', "=", "point_items.item_id")
-    .where("point_items.point_id", pointId)
-    .select("items.title")
+    const items = await knex("items")
+      .join("point_items", "items.id", "=", "point_items.item_id")
+      .where("point_items.point_id", pointId)
+      .select("items.title");
 
     if (!point) {
       return response.status(400).json({ message: "Point not found." });
     }
 
-    return response.json({point, items});
+    return response.json({ point, items });
   }
 
   async create(request: Request, response: Response) {
     const { name, email, whatsapp, latitude, longitude, city, uf, items } =
       request.body;
+
+    const trx = await knex.transaction();
 
     const point = {
       image: "image-fake",
@@ -32,7 +34,7 @@ class PointsController {
       city,
       uf,
     };
-    const insertedIds = await knex("points").insert(point);
+    const insertedIds = await trx("points").insert(point);
     const point_id = insertedIds[0];
 
     const pointItems = items.map((item_id: number) => {
@@ -42,7 +44,9 @@ class PointsController {
       };
     });
 
-    await knex("point_items").insert(pointItems);
+    await trx("point_items").insert(pointItems);
+
+    await trx.commit();
 
     return response.json({
       id: point_id,
@@ -51,7 +55,19 @@ class PointsController {
   }
 
   async index(request: Request, response: Response) {
-    const points = await knex("points").select("*");
+    const { city, uf, items } = request.query;
+
+    const parsedItems = String(items)
+      .split(",")
+      .map((item) => Number(item.trim()));
+
+    const points = await knex("points")
+      .join("point_items", "points.id", "=", "point_items.point_id")
+      .whereIn("point_items.item_id", parsedItems)
+      .where("city", String(city))
+      .where("uf", String(uf))
+      .distinct()
+      .select("points.*");
 
     return response.json(points);
   }
