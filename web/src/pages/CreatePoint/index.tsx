a5a5d32ec0ff70eebee22c30a5386b190a40fa8d
Gabriel_Saliba <gabriel.saliba@posher.app>
2022-03-01 18:28:38 -0300

    🔧 - added env variables

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -73,27 +73,45 @@ const CreatePoint = () => {
 
           <ul className="items-grid">
             <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
+              <img
+                src={`${process.env.REACT_APP_API_URL}/uploads/oleo.svg`}
+                alt="Teste"
+              />
               <span>Óleo de Cozinha</span>
             </li>
             <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
+              <img
+                src={`${process.env.REACT_APP_API_URL}/uploads/oleo.svg`}
+                alt="Teste"
+              />
               <span>Óleo de Cozinha</span>
             </li>
             <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
+              <img
+                src={`${process.env.REACT_APP_API_URL}/uploads/oleo.svg`}
+                alt="Teste"
+              />
               <span>Óleo de Cozinha</span>
             </li>
             <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
+              <img
+                src={`${process.env.REACT_APP_API_URL}/uploads/oleo.svg`}
+                alt="Teste"
+              />
               <span>Óleo de Cozinha</span>
             </li>
             <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
+              <img
+                src={`${process.env.REACT_APP_API_URL}/uploads/oleo.svg`}
+                alt="Teste"
+              />
               <span>Óleo de Cozinha</span>
             </li>
             <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
+              <img
+                src={`${process.env.REACT_APP_API_URL}/uploads/oleo.svg`}
+                alt="Teste"
+              />
               <span>Óleo de Cozinha</span>
             </li>
           </ul>
